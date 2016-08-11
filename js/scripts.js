88f618f65ab340c24452cd2d6a43d0d1609801df
Kyle Lange and Marta Sánchez <kyle@>
2016-08-11 09:40:52 -0700

    add javascript interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,12 @@
 // User Interface Logic
 $(document).ready(function(){
   var height = parseInt(prompt("How tall are you?"));
-  if (height <= 45) {
-    ("#carousel").show();
-  } else if (height >= 60) {
-    ("#coaster").show();
+  if (height < 45) {
+    alert (height);
+    $("#carousel").show();
+  } else if (height > 60) {
+    $("#coaster").show();
   } else {
-    ("#ship").show();
-  }
-
-  }
-  }
-
+    $("#ship").show();
   };
 });
